467d81255d0644175ca3474d00292c6fa378f93a
Ahmad-Al-Ali <ahmad.alali@outlook.com>
2019-06-24 18:25:40 +0300

    create table

--- index.php
+++ index.php
@@ -4,33 +4,27 @@
          $this->open('test.db');
       }
    }
-   
    $db = new MyDB();
-   if(!$db){
+   if(!$db) {
       echo $db->lastErrorMsg();
    } else {
       echo "Opened database successfully\n";
    }
 
    $sql =<<<EOF
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (1, 'Paul', 32, 'California', 20000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (2, 'Allen', 25, 'Texas', 15000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
+      CREATE TABLE COMPANY
+      (ID INT PRIMARY KEY     NOT NULL,
+      NAME           TEXT    NOT NULL,
+      AGE            INT     NOT NULL,
+      ADDRESS        CHAR(50),
+      SALARY         REAL);
 EOF;
 
    $ret = $db->exec($sql);
-   if(!$ret) {
+   if(!$ret){
       echo $db->lastErrorMsg();
    } else {
-      echo "Records created successfully\n";
+      echo "Table created successfully\n";
    }
    $db->close();
 ?>
