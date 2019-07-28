7ade2ef783b83ba2d0d4160f7cc1045a3b98480a
Ahmad Alali <ahmad.alali@outlook.com>
2019-07-28 13:12:12 +0300

    MySQL initial

--- index.php
+++ index.php
@@ -1,35 +1,27 @@
-<?
-phpinfo();
-?>
+
 <?php
-// $servername = "ah-db-server.mysql.database.azure.com";
-// $username = "ahmadalali@ah-db-server";
-// $password =  getenv("pass") // "Imsup3rawesome";
+$servername = "ah-db-server.mysql.database.azure.com";
+$username = "ahmadalali@ah-db-server";
+$password =  getenv("pass") // "Imsup3rawesome";
 
-// //$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "ah-db-server.mysql.database.azure.com", "ahmadalali@ah-db-server", {your_password}, {your_database}, 3306);
-// // Create connection
-// $conn = new mysqli($servername, $username, $password);
+//$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "ah-db-server.mysql.database.azure.com", "ahmadalali@ah-db-server", {your_password}, {your_database}, 3306);
+//Create connection
+$conn = new mysqli($servername, $username, $password);
 
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// } 
-// echo "Connected successfully";
+ // Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+ echo "Connected successfully";
 
 // // Create database
-// $sql = "CREATE DATABASE myDB";
-// if ($conn->query($sql) === TRUE) {
-//     echo "Database created successfully";
-// } else {
-//     echo "Error creating database: " . $conn->error;
-// }
+ $sql = "CREATE DATABASE myDB";
+ if ($conn->query($sql) === TRUE) {
+     echo "Database created successfully";
+ } else {
+     echo "Error creating database: " . $conn->error;
+ }
 
-// $conn->close();
-// ?>
+ $conn->close();
+ ?>
 
-
-// <?php
-
-// echo "Hello World!";
-
-// ?>
